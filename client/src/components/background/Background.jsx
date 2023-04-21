174695ed599a69ad43be29483cdecf084491abbe
Musire <Mcmusire@gmail.com>
2023-04-21 15:23:06 -0600

    Fix on the hover state of social media links

--- client/src/components/background/Background.jsx
+++ client/src/components/background/Background.jsx
@@ -1,13 +1,14 @@
 import { GrFacebook as Facebook, GrPinterest as Pinterest, GrInstagram as Instagram  } from "react-icons/gr";
 
 const Background = () => {
+    const linkStyle = 'mobile:w-8 mobile:h-8 desktop:w-7 desktop:h-7 fill-grayblue hover:fill-softred easy-transition'
     return ( 
         <div className="background-stars" >
             <div className="background-hills">
-                <div className="w-40 spaced">
-                    <Facebook className="mobile:h-8 mobile:w-8 desktop:w-7 desktop:h-7 fill-grayblue" />
-                    <Pinterest className="mobile:w-8 mobile:h-8 desktop:w-7 desktop:h-7 fill-grayblue" />
-                    <Instagram className="mobile:w-8 mobile:h-8 desktop:w-7 desktop:h-7 fill-grayblue" />
+                <div className="z-50 w-40 spaced">
+                    <Facebook className={linkStyle} />
+                    <Pinterest className={linkStyle} />
+                    <Instagram className={linkStyle} />
                 </div>
             </div>
         </div>
